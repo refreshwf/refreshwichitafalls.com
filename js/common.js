028d058862ac90817e6dc13cf06e9d6991fd6658
James Fleeting <twofivethreetwo@gmail.com>
2012-04-23 22:40:14 -0500

    initial commit

---
+++ js/common.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+	$('.section-container').each(function(i) {
+		var position = $(this).position();
+		$(this).scrollspy({
+			min: position.top,
+			max: position.top + $(this).height(),
+			onEnter: function(element, position) {
+				$('nav li a').removeClass('current');
+				$('nav li.nav-'+element.id+' a').addClass('current');
+			},
+			onLeave: function(element, position) {
+				$('nav li.nav-'+element.id+' a').removeClass('current');
+			}
+		});
+	});
+
+
+
+	(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) return;
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=351107928267280";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+
+	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+
+	(function() {
+		var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+		po.src = 'https://apis.google.com/js/plusone.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+	})();
+
+	var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+	g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+	s.parentNode.insertBefore(g,s)}(document,'script'));
+});
---
+++ readme.md
@@ -0,0 +1,9 @@
+# refreshwichitafalls.com
+
+This repo is the home of Refresh WF which is powered by [Jekyll](http://jekyllrb.com/) and [GitHub](http://github.com). The starting point for this site was [Jekyll Bootstrap](http://jekyllbootstrap.com/).
+
+## License
+
+The directories `_posts/`, `_drafts` and `images/` and their contents are Copyright James Fleeting. You may not reuse anything therein without permission.
+
+All other directories and files are MIT Licensed including the HTML and CSS. You are free to clone this repo as a starting point for your own Jekyll install or take pieces you want.
