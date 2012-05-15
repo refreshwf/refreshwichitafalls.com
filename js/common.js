29d5f04bba8de5a3938311531345ffd2d1afa6f2
James Fleeting <hello@jamesfleeting.com>
2012-05-14 20:15:35 -0500

    added Google+ badge, animated page load

--- js/common.js
+++ js/common.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
+	$('nav').delay(800).slideDown(400, function() {
+		$('header').delay(300).fadeIn(600, function() {
+			$('.upcoming-event').slideDown(600, function() {
+				$('.main').fadeIn(800);
+				$('footer').fadeIn(600);
+			});
+		});
+	});
+
 	$('.section-container').each(function(i) {
 		var position = $(this).position();
 		$(this).scrollspy({
@@ -14,8 +23,7 @@ $(document).ready(function() {
 		});
 	});
 
-
-
+	// Facebook
 	(function(d, s, id) {
 		var js, fjs = d.getElementsByTagName(s)[0];
 		if (d.getElementById(id)) return;
@@ -24,11 +32,14 @@ $(document).ready(function() {
 		fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));
 
+	// Twitter
 	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 
+	// Google+
+	window.___gcfg = {lang: 'en'};
 	(function() {
-		var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-		po.src = 'https://apis.google.com/js/plusone.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+		var po = document.createElement("script");
+		po.type = "text/javascript"; po.async = true; po.src = "https://apis.google.com/js/plusone.js";
+		var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(po, s);
 	})();
 });
